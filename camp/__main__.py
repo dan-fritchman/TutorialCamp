aeef94eafd34b422ad382a5addaaf1aba8b4bd05
Dan Fritchman <dan@fritch.mn>
2023-12-11 12:40:14 -0800

    __main__-ize

---
+++ camp/__main__.py
@@ -0,0 +1,13 @@
+"""
+# `camp` Main Entry Point
+
+Usually invoked by running 
+```
+python -m camp
+```
+"""
+
+from .mos_char import main as mos_char_main
+
+if __name__ == "__main__":
+    mos_char_main()
